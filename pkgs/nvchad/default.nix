d0e5736aeb478b6b8518aca4dcc4f2ce29e2aeb4
hizuhi <sunzhiye8@gmail.com>
2024-12-18 17:25:36 +0800

    修改nvchad

--- pkgs/nvchad/default.nix
+++ pkgs/nvchad/default.nix
@@ -1,21 +1,19 @@
-{ stdenv, pkgs, fetchFromGithub, ...}:
+{ stdenv, pkgs, fetchFromGitHub, ...}:
 
-{
-  nvchad = stdenv.mkDerivation rec {
-    name = "nvchad";
-    version = "";
-    dontBuild = true;
+stdenv.mkDerivation rec {
+  name = "nvchad";
+  version = "";
+  dontBuild = true;
 
-  src = pkgs.fetchFromGithub {
+  src = pkgs.fetchFromGitHub {
     owner = "NvChad";
     repo = "NvChad";
-    rev = "v2.5";
-    sha256 = "";
+    rev = "c8777040fbda6a656f149877b796d120085cd918";
+    sha256 = "sha256-J4SGwo/XkKFXvq+Va1EEBm8YOQwIPPGWH3JqCGpFnxY=";
   };
 
   installPhase = ''
     mkdir $out
     cp -aR $src/* $out/
   '';
-  };
 }
